9bb28fa8378d63a1ac1006f69dc0bab4592033e8
iamhrk <iamhrk8@gmail.com>
2023-05-27 10:44:04 +0530

    Add post create component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+
+  newPost= "";
+  onAddPost(){
+    this.newPost = 'Hi my new post';
+  }
+
+}
