8db987e3858cb686b60f133cc1aee8e94b176ebb
iamhrk <iamhrk8@gmail.com>
2023-05-30 09:56:40 +0530

    Fetch posts from server side

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first middleware');
-  next();
-});
+app.use("/api/posts", (req, res, next) => {
+  const posts = [{
+    id : "dfhasdfklj2jk1212",
+    title : "First server side post",
+    content: "First server side post content"
+  },
+  {
+    id: "fadjkldjakfjakj1214",
+    title: "Second server side post",
+    content: "Second server side post content"
+  }
+];
 
-app.use((req, res, next) => {
-  res.send("hello from express");
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts: posts
+  });
 });
 
 module.exports = app;
