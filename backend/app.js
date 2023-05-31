e3ae9912e86652942be982ebc1a62c929511c1c3
iamhrk <iamhrk8@gmail.com>
2023-05-31 14:37:43 +0530

    Connect to database and fetch posts

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const Post = require('./model/post');
 const app = express();
+
+mongoose.connect(/*secret mongodb credential here*/)
+  .then( () => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Database connection failed!");
+  })
 
 app.use(bodyParser.json());
 
@@ -13,29 +23,23 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
-    message: "response send successfully  0gBZN4ozTgBaBME0"
+    message: "response send successfully"
   })
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [{
-    id : "dfhasdfklj2jk1212",
-    title : "First server side post",
-    content: "First server side post content"
-  },
-  {
-    id: "fadjkldjakfjakj1214",
-    title: "Second server side post",
-    content: "Second server side post content"
-  }
-];
 
-  res.status(200).json({
-    message: "Posts fetched successfully",
-    posts: posts
+  Post.find().then( documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      posts: documents
+    });
   });
 });
 
