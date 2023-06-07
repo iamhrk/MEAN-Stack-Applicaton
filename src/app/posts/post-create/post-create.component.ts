cbc2699aed498fa56c80bea92cd9aceb4aebe8ab
iamhrk <iamhrk8@gmail.com>
2023-06-07 09:58:45 +0530

    Add image preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit{
   enteredContent = "";
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId : string;
   post: Post;
@@ -55,8 +56,11 @@ export class PostCreateComponent implements OnInit{
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({ image: file });
     this.form.get('image').updateValueAndValidity();
-    console.log(file);
-    console.log(this.form);
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    }
+    reader.readAsDataURL(file);
   }
 
   onSavePost(){
